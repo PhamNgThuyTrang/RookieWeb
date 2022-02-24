94d4490538d94ca93f22dd46ebce6c60114d595e
trangpham <trang.pham@kyanon.digital>
2022-02-24 10:09:11 +0700

    Create HomePage with template "_Layout" and add Banner to HomePage

---
+++ Rookie.CustomerSite/Mapping/BannerAutoMapperProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Rookie.CustomerSite.ViewModel;
+using Rookie.CustomerSite.ViewModel.Banner;
+using RookieShop.Shared.Dto;
+using RookieShop.Shared.Dto.Banner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rookie.CustomerSite.Mapping
+{
+    public class BannerAutoMapperProfile : Profile
+    {
+        public BannerAutoMapperProfile()
+        {
+            CreateMap<BannerDto, BannerVm>().ReverseMap();
+            CreateMap<BaseQueryCriteriaDto, BaseQueryCriteriaVM>().ReverseMap();
+            CreateMap<PagedResponseDto<BannerDto>, PagedResponseVM<BannerVm>>().ReverseMap();
+        }
+    }
+}
---
+++ Rookie.CustomerSite/ViewComponents/BannerViewComponent.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Rookie.CustomerSite.ViewModel;
+using Rookie.CustomerSite.ViewModel.Banner;
+using RookieShop.Shared.Constants;
+using RookieShop.Shared.Dto.Banner;
+using RookieShop.Shared.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rookie.CustomerSite.ViewComponents
+{
+    public class BannerViewComponent : ViewComponent
+    {
+        private readonly IBannerService _bannerService;
+        private readonly ISubCategoryService _subCategoryService;
+        private readonly IConfiguration _config;
+        private readonly IMapper _mapper;
+
+        public BannerViewComponent(
+            IBannerService bannerService,
+            IConfiguration config,
+            IMapper mapper)
+        {
+            _bannerService = bannerService;
+            _config = config;
+            _mapper = mapper;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var bannerCriteriaDto = new BannerCriteriaDto()
+            {
+                SortOrder = SortOrderEnum.Accsending,
+                Limit = int.Parse(_config[ConfigurationConstants.PAGING_LIMIT])
+            };
+            var pagedbanners = await _bannerService.GetBannerAsync(bannerCriteriaDto);
+            var banners = _mapper.Map<PagedResponseVM<BannerVm>>(pagedbanners);
+            return View(banners);
+        }
+    }
+}
