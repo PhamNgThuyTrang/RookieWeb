4731542eba4471dc1449b95d37e462bf6c6404c6
trangpham <pntt220200@gmail.com>
2022-03-09 17:12:23 +0700

    refactor

---
+++ Rookie.CustomerSite/ViewComponents/ListProductViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rookie.CustomerSite.ViewModel;
+using Rookie.CustomerSite.ViewModel.Product;
+using System.Threading.Tasks;
+
+namespace Rookie.CustomerSite.ViewComponents
+{
+    public class ListProductViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(PagedResponseVM<ProductVm> products)
+        {
+            return View(products);
+        }
+    }
+}
---
+++ Rookie.CustomerSite/ViewComponents/PaginationViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rookie.CustomerSite.ViewModel;
+using Rookie.CustomerSite.ViewModel.Product;
+using System.Threading.Tasks;
+
+namespace Rookie.CustomerSite.ViewComponents
+{
+    public class PaginationViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(PagedResponseVM<ProductVm> pagedProducts)
+        {
+            return View(pagedProducts);
+        }
+    }
+}
